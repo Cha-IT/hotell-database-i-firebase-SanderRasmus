560ea044baad3f2884253eb5becb5479ab63f69d
SanderRasmus <sanderrasmussen@outlook.com>
2024-04-30 12:34:32 +0200

    Code change: Selektorer er nå implementert.

--- hotel.js
+++ hotel.js
@@ -11,68 +11,87 @@ import {
 import { db } from "./firebaseConfig.js";
 import { HotelRom } from "./romClass.js";
 
+/* Henter inn alle HTML Elementer */
 const RomContainerEl = document.querySelector("#Rom-Container");
-const FilterBtns = document.querySelectorAll(".FilterBtn");
+const EtasjeFilter = document.querySelector("#Etasje-Filter");
+const SengeplassFilter = document.querySelector("#Sengeplass-Filter");
+const RomTypeFilter = document.querySelector("#RomType-Filter");
+const TilgjengeligFilter = document.querySelector("#Tilgjengelig-Filter");
 
-FilterBtns.forEach((button) => {
-  button.addEventListener("click", HentRom);
+/* Lager en egen array over mine elementer og bruker en for-løkke til eventlistener */ 
+[EtasjeFilter, SengeplassFilter, RomTypeFilter, TilgjengeligFilter].forEach((selektor) => {
+  selektor.addEventListener("change", hentData);
 });
 
-async function HentRom(e) {
-  let Valgtfilter = e.target;
+/* Asynkron kode som henter ut data */
+async function hentData() {
+  const etasje = EtasjeFilter.value;
+  const senger = SengeplassFilter.value;
+  const romtype = RomTypeFilter.value;
+  const tilgjengelig = TilgjengeligFilter.value;
 
-  const databaseSnapshot = await getDocs(collection(db, "hotelrom"));
+  /* Filter Array som lagrer hvilke filtere vi benytter oss av og dens data */
+  const filtere = [];
+  
+  /* Så lenge Etasje ikke har empty value pusher den filteret med etasje valuen 1 eller 2. 
+  Dette kommer som string så vi endrer det til integer for å passe med databasen's data */
+  if (etasje !== "") {
+    filtere.push(where("Etasje", "==", parseInt(etasje)));  
+  }
+  
+  /* Samme Prosess som over */
+  if (senger !== "") {
+    if(senger == "3"){
+      filtere.push(where("Sengeplasser", ">=", parseInt(senger)));
+    } else {
+      filtere.push(where("Sengeplasser", "==", parseInt(senger)));
+    }
+  }
 
+  /* Samme Prosess som over uten parsing siden hoteltype er en string */
+  if (romtype !== "") {
+    filtere.push(where("Type", "==", romtype));
+  }
+
+  /* Samme Prosess som over men med isAvailble siden den er boolean */
+  if (tilgjengelig !== "") {
+    const isAvailable = tilgjengelig === "1"; 
+    filtere.push(where("Ledig", "==", isAvailable));
+  }
+
+  // Lager en spørring ved å kombinere alle filtrene som er valgt.
+  // Den starter med samlingen "hotelrom" fra databasen,
+  // og legger til hvert filter for å begrense resultatene etter behov.
+  const filtrertQuery = filtere.reduce((acc, filter) => query(acc, filter), collection(db, "hotelrom"));
+  const querySnapshot = await getDocs(filtrertQuery);
+
+  /* Hvis filter array lengden er større en 0 blir dataen vist på skjermen */
+  if(filtere.length > 0) {
+    visData(querySnapshot);
+  }
+
+  /* Er null filtere valgt fjerner den alt som blir visst */
+  if(filtere.length == 0) {
+    while (RomContainerEl.firstChild) {
+      RomContainerEl.removeChild(RomContainerEl.firstChild);
+    }
+  }
+}
+
+// Vis data funksjonen
+function visData(querySnapshot) {
+  // Fjerner alt først før den viser slik at gammel data blir bort
   while (RomContainerEl.firstChild) {
     RomContainerEl.removeChild(RomContainerEl.firstChild);
   }
 
-  if (Valgtfilter.id === "Etasje1") {
-    const q = query(collection(db, "hotelrom"), where("Etasje", "==", 1));
-    const querySnapshot = await getDocs(q);
-
-    databaseFilter(querySnapshot);
-  }
-
-  if (Valgtfilter.id === "Etasje2") {
-    const q = query(collection(db, "hotelrom"), where("Etasje", "==", 2));
-    const querySnapshot = await getDocs(q);
-
-    databaseFilter(querySnapshot);
-  }
-
-  if (Valgtfilter.id === "1Seng") {
-    const q = query(collection(db, "hotelrom"), where("Sengeplasser", "==", 1));
-    const querySnapshot = await getDocs(q);
-
-    databaseFilter(querySnapshot);
-  }
-
-  if (Valgtfilter.id === "2Seng") {
-    const q = query(collection(db, "hotelrom"), where("Sengeplasser", ">=", 2));
-    const querySnapshot = await getDocs(q);
-
-    databaseFilter(querySnapshot);
-  }
-
-  if (Valgtfilter.id === "AlleRom") {
-    databaseSnapshot.forEach((rom) => {
-      const romData = rom.data();
-      new HotelRom(romData);
-    });
-  }
-
-  if (Valgtfilter.id === "LedigeRom") {
-    const q = query(collection(db, "hotelrom"), where("Ledig", "==", true));
-    const querySnapshot = await getDocs(q);
-
-    databaseFilter(querySnapshot);
-  }
-}
-
-function databaseFilter(querySnapshot) {
+  // Dynamisk lager dataen ved bruk av hotelrom klassen. Querysnapshot er begrepet brukt
+  // For å definere dataen som vi henter ut fra databasen.
   querySnapshot.forEach((rom) => {
+    // romData er dataen vi hvert dokument i databasen
     const romData = rom.data();
+    // Vi sender det videre inn til hotelrom klassen slik at den kan ta bruk av dataen
+    // Når den genererer / dynamisk viser informasjon.
     new HotelRom(romData);
   });
 }
